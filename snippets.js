597be08670080439277b3dd1dd0466b3dcef3e86
Thomas <33089253+tantum-code@users.noreply.github.com>
2021-07-19 23:25:10 +0200

    Changed Accelerometer values

--- snippets.js
+++ snippets.js
@@ -1,6 +1,44 @@
 /****************** GPS **************************/
 var x = document.getElementById("p_geoloc");
 
+function getAccel(){
+    DeviceMotionEvent.requestPermission().then(response => {
+        if (response == 'granted') {
+            console.log("accelerometer permission granted");
+            // Do stuff here
+            window.addEventListener('deviceorientation',(event) => {
+                // Expose each orientation angle in a more readable way
+            rotation_degrees = event.alpha;
+            frontToBack_degrees = event.beta;
+            leftToRight_degrees = event.gamma;
+                
+                // Update velocity according to how tilted the phone is
+                // Since phones are narrower than they are long, double the increase to the x velocity
+            vx = vx + leftToRight_degrees * updateRate*2; 
+            vy = vy + frontToBack_degrees * updateRate;
+                
+                // Update position and clip it to bounds
+            px = px + vx*.5;
+            if (px > 98 || px < 0){ 
+                px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
+                vx = 0;
+            }
+
+            py = py + vy*.5;
+            if (py > 98 || py < 0){
+                py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
+                vy = 0;
+            }
+                
+            dot = document.getElementsByClassName("indicatorDot")[0]
+            dot.setAttribute('style', "left:" + (px) + "%;" +
+                                          "top:" + (py) + "%;");
+            
+        });
+    }
+});
+}
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
